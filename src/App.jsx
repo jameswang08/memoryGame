30b5a7f31dcf8a376a2c1bf8a7e8ac72a335be3d
James Wang <114769089+jameswang08@users.noreply.github.com>
2023-09-06 21:29:03 -0700

    Implement function to fetch pokemon sprites from PokeAPI and display the sprites

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [spriteList, setSpriteList] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+
+  async function getSprites(){
+    //Get pokedex and extract list of pokemon
+    try{
+      const rawData = await fetch("https://pokeapi.co/api/v2/pokedex/kanto/");
+      const pokedex = await rawData.json();
+      const pokeList = pokedex.pokemon_entries;
+      //Get corresponding sprite for each pokemon
+      const sprites = await Promise.all(
+        pokeList.map(async (item) => {
+          try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${item.pokemon_species.name}/`);
+            const pokemon = await response.json();
+            return {link: pokemon.sprites.front_default, clicked: false};
+          } catch (error) {
+            console.error("Error:", error);
+            return null; 
+          }
+        })
+      );
+      setSpriteList(sprites);
+    }
+    catch(error){
+      console.log("Error:", error);
+    }
+  }
+
+
+
+  useEffect(() => {
+    getSprites();
+  },[]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='score'>
+        <h1>Score: {score}</h1>
+        <h1>High Score: {highScore}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {spriteList.map((item, index) => {
+        return <img src={item.link} alt="Image of a Pokemon" key={index}></img>
+      })}
     </>
-  )
+  );
 }
-
 export default App
